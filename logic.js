fc10c97b63e12a97036a31de94315827d2736542
yangjie500 <kristeh97@gmail.com>
2021-08-30 23:06:14 +0800

    Finalised

--- logic.js
+++ logic.js
@@ -1,64 +1,121 @@
-function add(a,b) {
-    return a + b;
+const numBtn = document.querySelectorAll('.btn');
+const opBtn = document.querySelectorAll('.btn-b');
+const clearBtn = document.querySelectorAll('.btn-c');
+const equalBtn = document.querySelector('.btn-d')
+const mainDisplay = document.querySelector('.current-number');
+const subDisplay = document.querySelector('.memory');
+
+function rounding(number) {
+    number = parseFloat((Math.round(number + "e+2")  + "e-2"))
+    
+    if (number.toString().length > 8) {
+        return number.toExponential(2)
+    }
+    console.log(number)
+    return number
 }
 
-function minus(a,b) {
-    return a - b;
+function calculate(e, numberFirst, numberSecond, operator) {
+
+    numberFirst = parseFloat(numberFirst);
+    numberSecond = parseFloat(numberSecond);
+
+    switch (operator) {
+        case "+" :
+            return ["", rounding(numberFirst + numberSecond).toString()];
+        case "-" :
+            return ["", rounding(numberFirst - numberSecond).toString()];
+        case "×" :
+            return ["", rounding(numberFirst * numberSecond).toString()];
+        case "÷" :
+            return ["", rounding(numberFirst / numberSecond).toString()];
+        case "^" :
+            return ["", rounding(Math.pow(numberFirst, numberSecond)).toString()];
+    }
+    
 }
 
-function multiply(a,b) {
-    return a * b;
+function displayNum(e, number1, number2, operator) {
+    if (e.target.textContent === "=") {
+        mainDisplay.textContent = number1;
+        subDisplay.textContent = number2;
+        return
+    }
+    mainDisplay.textContent = number1;
+    if (operator !== "") {
+        subDisplay.textContent = `${number2} ${operator}`;
+        return
+    }
+    subDisplay.textContent = number2;
 }
 
-function divide(a,b) {
-    if (b===0) {
-        return 'NaN'
-    } 
-
-    return a / b;
+function clear(e, number1, number2, operator) {
+    if (e.target.textContent === 'AC') {
+        number1 = "";
+        number2 = "";
+        operator = "";
+        return [number1, number2, operator];
+    } else {
+       return [number1.substring(0, mainDisplay.textContent.length - 1), number2, operator];
+    }
 }
 
-function power(a,b) {
-    return Math.pow(a,b);
+function appendNum(e, number) {
+    if (number.includes('.') && e.target.textContent === '.') return number;
+    number = number + e.target.textContent;
+    return number;
+    
 }
 
-function operate(a,b, operate) {
-}
+function getOperator(e, number1, number2, prevOperator) {
+    const op = e.target.textContent;
 
-function getNumber(e, numbers) {
-    const name = e.target.classList[1];
-    const number = name.substring(5);
-    numbers.push(number);
-    console.log(numbers)
+    if (number1 === "" && number2 !== "") return [number1, number2, op];
 
-}
+    if (number1 === "") return [number1, number2, ""]
 
-function getOperation(e, numbers) {
-    const name = e.target.classList[1];
-    const operation = name.substring(5);
-    console.log(numbers);
-}
+    if (number2 !== "" && number1 !== "") {
+        console.log(number1)
+        console.log(number2)    
+        console.log(prevOperator);
+        [number1, number2] = calculate(e, number2, number1, prevOperator);
+        return [number1, number2, op];
+    }
 
-function displayMain(num) {
-    const mainScreen = document.querySelector(".current-number");
-    mainScreen.textContent = num;
+    return ["", number1, op]
 }
 
 function logic() {
-    const buttonsA = document.querySelectorAll(".btn");
-    const buttonsB = document.querySelectorAll(".btn-b");
-    const buttonsC = document.querySelectorAll(".btn-c");
-    const numbers = [];
+    let currentOperand = ""; 
+    let prevOperand = "";
+    let operator = "";
 
-    buttonsA.forEach((elem) => {
-        elem.addEventListener('click', (e) => {getNumber(e, numbers)});
-
-    });
-    buttonsB.forEach((elem) => {
-        elem.addEventListener('click', (e) => {getOperation(e, numbers)});
+    numBtn.forEach((elem) => {
+        elem.addEventListener('click', (e) => {
+            currentOperand = appendNum(e, currentOperand);
+            displayNum(e, currentOperand, prevOperand, operator);
+        })
     });
 
+    clearBtn.forEach((elem) => {
+        elem.addEventListener('click', (e) => {
+            [currentOperand, prevOperand, operator] = clear(e, currentOperand, prevOperand, operator);
+            displayNum(e, currentOperand, prevOperand, operator);
+        })
+    })
 
+    opBtn.forEach((elem) => {
+        elem.addEventListener('click', (e) => {
+            [currentOperand, prevOperand, operator] = getOperator(e, currentOperand, prevOperand, operator);
+            displayNum(e, currentOperand, prevOperand, operator);
+        })
+    })
+
+    equalBtn.addEventListener('click', (e) => {
+        [currentOperand, prevOperand] = calculate(e, prevOperand, currentOperand, operator)
+        displayNum(e, prevOperand, currentOperand, "")
+    })
 }
+
 
 window.onload = logic();
